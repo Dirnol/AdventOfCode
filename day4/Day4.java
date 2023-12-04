be946635826ff35ba4bf7ade2576ca3f67fd385f
dirnol <dirnol@yahoo.com>
2023-12-04 10:26:22 -0600

    Day 4 Completed

--- day4/Day4.java
+++ day4/Day4.java
@@ -2,6 +2,7 @@ package adventofcode.day4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day4 {
@@ -26,13 +27,40 @@ public class Day4 {
 			e.printStackTrace();
 		}
 		
+		int total = 0;
+		
 		while(scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			
-			//Do stuff with the input here
+			int pointVal = 0;
+			
+			ArrayList<Integer> winningNumbers = new ArrayList<>();
+			ArrayList<Integer> yourNumbers = new ArrayList<>();
+			
+			String[] winningNumbersStrings = line.split("\\|")[0].split(":")[1].trim().split(" ");
+			String[] yourNumbersStrings = line.split("\\|")[1].trim().split(" ");
+			
+			for(String s : winningNumbersStrings)
+				if(!s.trim().isEmpty())
+					winningNumbers.add(Integer.parseInt(s));
+			
+			for(String s : yourNumbersStrings)
+				if(!s.trim().isEmpty())
+					yourNumbers.add(Integer.parseInt(s));
+			
+			for(int num : yourNumbers) {
+				if(winningNumbers.contains(num)) {
+					if(pointVal == 0)
+						pointVal = 1;
+					else
+						pointVal *= 2;
+				}
+			}
+			total += pointVal;
 			
 		}
 		
+		solution = total + "";
 		scanner.close();
 		return solution;
 	}
@@ -41,19 +69,60 @@ public class Day4 {
 		
 		String solution = "N/A";
 		
+		int[] cardCopies = new int[204];
+		for(int i = 0;  i < cardCopies.length; i++) {
+			cardCopies[i] = 1;
+		}
+		
+		
 		try {
 			scanner = new Scanner(new File("src/adventofcode/day"+ day +"/input01.txt"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
+		int total = 0;
+		
 		while(scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			
-			//Do stuff with the input here
+			int cardNum = Integer.parseInt(line.split(":")[0].substring(4).trim())-1;
+			
+			int winNum = 0;
+			
+			ArrayList<Integer> winningNumbers = new ArrayList<>();
+			ArrayList<Integer> yourNumbers = new ArrayList<>();
+			
+			String[] winningNumbersStrings = line.split("\\|")[0].split(":")[1].trim().split(" ");
+			String[] yourNumbersStrings = line.split("\\|")[1].trim().split(" ");
+			
+			for(String s : winningNumbersStrings)
+				if(!s.trim().isEmpty())
+					winningNumbers.add(Integer.parseInt(s));
+			
+			for(String s : yourNumbersStrings)
+				if(!s.trim().isEmpty())
+					yourNumbers.add(Integer.parseInt(s));
+			
+			for(int num : yourNumbers) {
+				if(winningNumbers.contains(num)) {
+					winNum++;
+				}
+			}
+			
+			for(int r = 0; r < cardCopies[cardNum]; r++) {
+				for(int i = cardNum+1; i < cardNum+1+winNum; i++) {
+					cardCopies[i]++;
+				}
+			}
 			
 		}
 		
+		for(int i : cardCopies) {
+			total += i;
+		}
+		
+		solution = total + "";
 		scanner.close();
 		return solution;
 	}
