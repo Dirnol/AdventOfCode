27440d6ae4eb34b64ac30bad2f745ab2be8a6041
dirnol <dirnol@yahoo.com>
2023-12-06 08:11:57 -0600

    Day 6 Completed

--- day6/Day6.java
+++ day6/Day6.java
@@ -2,6 +2,7 @@ package adventofcode.day6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day6 {
@@ -15,6 +16,10 @@ public class Day6 {
 		System.out.println("Part 1: " + partOne());
 		System.out.println("Part 2: " + partTwo());
 	}
+
+	private boolean isDigit(String line, int index) {
+		return line.charAt(index) >= '0' && line.charAt(index) <= '9';
+	}
 	
 	private String partOne() {
 		
@@ -26,15 +31,51 @@ public class Day6 {
 			e.printStackTrace();
 		}
 		
+		ArrayList<Integer> time = new ArrayList<>();
+		ArrayList<Integer> distance = new ArrayList<>();
+		
 		while(scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			
-			//Do stuff with the input here
+			if(line.startsWith("Time"))
+				readValues(line, time);
 			
+			if(line.startsWith("Distance"))
+				readValues(line, distance);
 		}
 		
+		int total = 1;
+		for(int i = 0; i < time.size(); i++) {
+			int wins = 0;
+			for(int t = 0; t < time.get(i); t++) {
+				int speed = t;
+				int travelTime = time.get(i) - t;
+				int totalDistance = travelTime * speed;
+				if(totalDistance > distance.get(i))
+					wins++;
+			}
+			total *= wins;
+		}
+		
+		solution = total + "";
 		scanner.close();
 		return solution;
+	}
+	
+	private void readValues(String line, ArrayList<Integer> out) {
+		String numPart = line.split(":")[1].trim();
+		String num = "";
+		for(int i = 0; i < numPart.length(); i++) {
+			if(isDigit(numPart, i))
+				num += numPart.charAt(i);
+			else {
+				if(!num.isEmpty()) {
+					out.add(Integer.parseInt(num));
+					num = "";
+				}
+			}
+		}
+		out.add(Integer.parseInt(num));
 	}
 	
 	private String partTwo() {
@@ -47,13 +88,29 @@ public class Day6 {
 			e.printStackTrace();
 		}
 		
+		long time =  0;
+		long distance = 0;
+		
 		while(scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			
-			//Do stuff with the input here
+			if(line.startsWith("Time"))
+				time = Long.parseLong(line.split(":")[1].replace(" ", ""));
 			
+			if(line.startsWith("Distance"))
+				distance = Long.parseLong(line.split(":")[1].replace(" ", ""));
 		}
 		
+		int wins = 0;
+		for(int t = 0; t < time; t++) {
+			long speed = t;
+			long travelTime = time - t;
+			long totalDistance = travelTime * speed;
+			if(totalDistance > distance)
+				wins++;
+		}
+		
+		solution = wins + "";
 		scanner.close();
 		return solution;
 	}
