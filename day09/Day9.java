65496aefd42719ff3a7a68d0996faaea580b8f43
dirnol <dirnol@yahoo.com>
2023-12-09 00:37:08 -0600

    Completed Day 9

--- day09/Day9.java
+++ day09/Day9.java
@@ -2,12 +2,13 @@ package adventofcode.day09;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day9 {
 
 	private Scanner scanner;
-	private int day = 9;
+	private String day = "09";
 	
 	public Day9() {
 		
@@ -26,13 +27,35 @@ public class Day9 {
 			e.printStackTrace();
 		}
 		
+		int total = 0;
 		while(scanner.hasNextLine()) {
-			String line = scanner.nextLine();
+			String[] line = scanner.nextLine().trim().split(" ");
+			int[] history = new int[line.length];
+			for(int i = 0 ; i < history.length; i++)
+				history[i] = Integer.parseInt(line[i]);
 			
-			//Do stuff with the input here
+			ArrayList<int[]> historyArr = new ArrayList<>();
+			historyArr.add(history);
+			boolean allZero = false;
+			while(!allZero) {
+				int[] next = new int[history.length-1];
+				allZero = true;
+				for(int i = 0; i < next.length; i++) {
+					next[i] = history[i+1] - history[i];
+					if(next[i] != 0)
+						allZero = false;
+				}
+				historyArr.add(next);
+				history = next;
+			}
+			int future = 0;
+			for(int i = historyArr.size()-2; i >= 0; i--) {
+				future = future + historyArr.get(i)[historyArr.get(i).length-1];
+			}
+			total += future;
 			
 		}
-		
+		solution = total + "";
 		scanner.close();
 		return solution;
 	}
@@ -47,13 +70,35 @@ public class Day9 {
 			e.printStackTrace();
 		}
 		
+		int total = 0;
 		while(scanner.hasNextLine()) {
-			String line = scanner.nextLine();
+			String[] line = scanner.nextLine().trim().split(" ");
+			int[] history = new int[line.length];
+			for(int i = 0 ; i < history.length; i++)
+				history[i] = Integer.parseInt(line[i]);
 			
-			//Do stuff with the input here
+			ArrayList<int[]> historyArr = new ArrayList<>();
+			historyArr.add(history);
+			boolean allZero = false;
+			while(!allZero) {
+				int[] next = new int[history.length-1];
+				allZero = true;
+				for(int i = 0; i < next.length; i++) {
+					next[i] = history[i+1] - history[i];
+					if(next[i] != 0)
+						allZero = false;
+				}
+				historyArr.add(next);
+				history = next;
+			}
+			int past = 0;
+			for(int i = historyArr.size()-2; i >= 0; i--) {
+				past = historyArr.get(i)[0] - past;
+			}
+			total += past;
 			
 		}
-		
+		solution = total + "";
 		scanner.close();
 		return solution;
 	}
